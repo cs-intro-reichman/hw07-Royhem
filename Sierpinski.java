c98a36310f28f7736af47fecf1c21880434fee8a
Roy hemelniker <roy.hemelniker@post.runi.ac.il>
2026-01-02 15:30:23 +0200

    completed assignment

--- Sierpinski.java
+++ Sierpinski.java
@@ -3,16 +3,51 @@ public class Sierpinski {
 	
 	public static void main(String[] args) {
 		sierpinski(Integer.parseInt(args[0]));
+		StdDraw.show();
 	}
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
-	}
+
+
+		double s = 1.0; 
+        double h = s * Math.sqrt(3) / 2; 
+        double x1 = 0;
+		double y1 = 0;          
+        double x2 = s;
+		double y2 = 0;         
+        double x3 = s / 2;
+		double y3 = h;     
+		StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.line(x1, y1, x2, y2);
+        StdDraw.line(x2, y2, x3, y3);
+        StdDraw.line(x3, y3, x1, y1);
+
+		sierpinski(n, x1, x2, x3, y1, y2, y3);
+
+}
 	
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+		if (n == 0) return;
+
+        double midX3 = (x3 + x1) / 2;
+        double midY3 = (y3 + y1) / 2;
+        double midX1 = (x1 + x2) / 2;
+        double midY1 = (y1 + y2) / 2;
+        double midX2= (x2 + x3) / 2;
+        double midY2= (y2 + y3) / 2;
+
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.line(midX1, midY1, midX2, midY2);
+        StdDraw.line(midX2, midY2, midX3, midY3);
+        StdDraw.line(midX3, midY3, midX1, midY1);
+        StdDraw.setPenColor(StdDraw.BLACK);
+
+       
+    	sierpinski(n - 1, x1, midX1, midX3, y1, midY1, midY3);
+    	sierpinski(n - 1, midX1, x2, midX2, midY1, y2, midY2);
+    	sierpinski(n - 1, midX3, midX2, x3, midY3, midY2, y3);
 	}
 }
